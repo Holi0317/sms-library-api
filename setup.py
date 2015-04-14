530e407902c222d3ea6a0d7cabf60a643f83ce6d
Holi0317 <holliswuhollis@gmail.com>
2015-04-14 21:36:05 +0800

    Fixed a typo, which have been there since the beginning

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
     name='sms-lib-helper',
     version='1.0',
-    pacakages=find_packages(),
+    packages=find_packages(),
     install_requires=[
         'click',
         'django',
