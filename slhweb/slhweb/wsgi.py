4aa45ab899e7fcd5a6dc53ca6c577cea09077196
Holi0317 <holliswuhollis@gmail.com>
2015-04-15 18:15:53 +0800

    Fixed Error 500 in production mode

--- slhweb/slhweb/wsgi.py
+++ slhweb/slhweb/wsgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "slhweb.settings")
+import sys
+os.environ['DJANGO_SETTINGS_MODULE'] = 'slhweb.settings'
+root_path = os.path.abspath(os.path.split(__file__)[0])
+sys.path.insert(0, os.path.join(root_path, '..'))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
