3e534d3c686ade800afd487a879fa62edcb104c6
Holi0317 <holliswuhollis@gmail.com>
2015-08-06 15:47:36 +0800

    Clean directory for prepration of porting

--- slhweb/src/js/animate-card.js
+++
@@ -1,8 +0,0 @@
-function animateCard(card, direction, position){
-  position = typeof position !== 'undefined' ? position : "-"+window.innerHeight+"px";
-  direction = typeof direction !== 'undefined' ? direction : "bottom";
-  card.css(direction, position);
-  card.addClass('animate-card');
-  card.show();
-  card.css(direction, "0");
-}
