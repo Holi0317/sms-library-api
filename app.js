f5113d4dbfe37a880ce65a94dcd00cd904bf761e
Holi0317 <holliswuhollis@gmail.com>
2015-09-08 07:04:34 +0800

    yo polymer

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var config = require('./config');
 var routes = require('./routes/index');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -21,6 +15,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.get('*', function(req, res){
+  res.status(404).send('Sorry. But I got a 404');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,7 +31,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function(err, req, res) {
     console.error(err);
     res.status(err.status || 500);
     res.render('error', {
@@ -46,7 +43,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
--- config.template.js
+++ config.template.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 
 module.exports = {
   // Mongoose createConnection instance
-  "conn": mongoose.createConnection('localhost/slh'),
+  conn: mongoose.createConnection('localhost/slh'),
 
   // Google AUTH
-  "clientId": "Client ID got from console.developer.google.com",
-  "clientSecret": "Client secret got from console.developer.google.com"
+  clientId: 'Client ID got from console.developer.google.com',
+  clientSecret: 'Client secret got from console.developer.google.com'
 };
