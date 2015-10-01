bd52ae2999c887dc51edbcba04fb52c660c682aa
Holi0317 <holliswuhollis@gmail.com>
2015-10-01 16:33:38 +0800

    Add model

--- models.js
+++ models.js
@@ -1,5 +1,35 @@
 'use strict';
 
-module.exports = {
+let mongoose = require('mongoose');
+let conn = require('./config').conn;
+let ObjectId = mongoose.Schema.Types.ObjectId;
 
+let schema = {
+  user: mongoose.Schema({
+    accessToken: String,
+    refreshToken: String,
+    libraryLogin: String,
+    libraryPassword: String,
+    enabled: Boolean,
+    logs: [{
+      type: ObjectId,
+      ref: 'Log'
+    }]
+  }),
+
+  log: mongoose.Schema({
+    time: Date,
+    user: {
+      type: ObjectId,
+      ref: 'User'
+    },
+    action: String
+  })
+};
+
+module.exports = {
+  user: conn.model('User', schema.user),
+  log: conn.model('Log', schema.log),
+
+  _schema: schema
 };
