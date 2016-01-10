131f03b7fc7c0b381c99554cdfa71fdf5826c1a3
Holi0317 <holliswuhollis@gmail.com>
2016-01-10 21:10:40 +0800

    Add documentation of utils.

--- backend/utils.js
+++ backend/utils.js
@@ -1,17 +1,46 @@
+/**
+ * Some utilities functions.
+ * @module backend/utils
+ * @author Holi0317 <holliswuhollis@gmail.com>
+ * @license MIT
+ *
+ * @requires bluebird
+ */
+
+'use strict';
+
 let Promise = require('bluebird');
 
-module.exports.BreakSignal = function BreakSignal() {
-  // Empty object prototype.
-  // This is used as a break signal to inform Promise that the chain has been breaked, and
-  // Exception is handled
-};
+/**
+ * The exception to be thrown when a promise is broken (have exception) and it is already handled.
+ * Useful when it comes to response handling as response cannot be sent twice.
+ * @example
+makePromise
+.then(throwRandomError)
+.catch(err => {
+  handleErr(err);
+  throw new BreakSignal();
+})
+.then(otherStuffs)
+.catch(err => {
+  if (err instanceof BreakSignal) return
+  handleErr2(err);
+});
+ * @class
+ */
+module.exports.BreakSignal = function BreakSignal() {};
 
+/**
+ * Supress all error when exception is thrown.
+ * @example
+makePromise()
+.then(throwError)
+.catch(catchThenThrowAnotherError)
+.then(otherStuffs)
+.catch(catchIgnore);
+// otherStuffs will not be executed, while there will be nothing printed on console.
+ * @returns {Promise} - emptyPromise
+ */
 module.exports.catchIgnore = function catchIgnore() {
-  /* Return a empty promise in all situation.
-   * Can be used to supress all error in a promise.
-   *
-   * E.g. makeAPromise().then(throwError).catch(catchThrowError).then(otherStuffs1).then(otherStuffs2).catch(catchIgnore)
-   * This will pass otherStuffs and end the promise chain, without showing ugly error on console.
-   */
   return Promise.resolve();
 };
