627bee34c8ac386aa55188ea109334765d690020
holi0317 <holliswuhollis@gmail.com>
2016-09-11 22:33:54 +0800

    FIX: clean task is noop

--- frontend/tasks/clean.js
+++ frontend/tasks/clean.js
@@ -2,6 +2,6 @@
 
 let gulp = require('gulp');
 let del = require('del');
+let {join} = require('path');
 
-// gulp.task('clean', del.bind(null, ['../.tmp', '../static']));
-gulp.task('clean', _=>_());
+gulp.task('clean', del.bind(null, [join(__dirname, '../.tmp'), join(__dirname, '../static')]));
