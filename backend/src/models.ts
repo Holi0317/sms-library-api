70cc93d01471ba418d7b39ae9b7af3fa221e6e23
holi0317 <holliswuhollis@gmail.com>
2016-09-26 22:28:01 +0800

    REF: backend/models.ts

--- backend/src/models.ts
+++ backend/src/models.ts
@@ -1,65 +1,72 @@
 import * as mongoose from 'mongoose';
-import {conn} from './config';
+import {config} from './config';
 
 mongoose.Promise = require('bluebird');
 
-let schema = {
-  user: new mongoose.Schema({
-    tokens: mongoose.Schema.Types.Mixed,
-    googleId: String,
-    libraryLogin: String,
-    libraryPassword: String,
-    renewDate: {
-      type: Number,
-      max: 13,
-      min: 2,
-      default: 3
-    },
-    renewEnabled: {
-      type: Boolean,
-      default: false
-    },
-    calendarEnabled: {
-      type: Boolean,
-      default: false
-    },
-    calendarName: {
+export enum LogLevels {
+  DEBUG,
+  INFO,
+  WARN,
+  ERROR,
+  FATAL,
+  SUCCESS
+}
+
+export let userSchema = new mongoose.Schema({
+  tokens: mongoose.Schema.Types.Mixed,
+  googleId: String,
+  libraryLogin: String,
+  libraryPassword: String,
+  renewDate: {
+    type: Number,
+    max: 13,
+    min: 2,
+    default: 3
+  },
+  renewEnabled: {
+    type: Boolean,
+    default: false
+  },
+  calendarEnabled: {
+    type: Boolean,
+    default: false
+  },
+  calendarName: {
+    type: String,
+    default: 'slh autorenew'
+  },
+  emailEnabled: {
+    type: Boolean,
+    default: false
+  },
+  emailAddress: String,
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  logs: [{
+    time: Date,
+    message: String,
+    level: {
       type: String,
-      default: 'slh autorenew'
-    },
-    emailEnabled: {
-      type: Boolean,
-      default: false
-    },
-    emailAddress: String,
-    isAdmin: {
-      type: Boolean,
-      default: false
-    },
-    logs: [{
-      time: Date,
-      message: String,
-      level: {
-        type: String,
-        enum: 'DEBUG INFO WARN ERROR FATAL SUCCESS'.split(' ')
-      }
-    }]
-  })
-};
+      enum: 'DEBUG INFO WARN ERROR FATAL SUCCESS'.split(' ')
+    }
+  }]
+});
 
 /**
  * Instance method.
  * Append a log entry into user document.
  * Does NOT perform save action.
  *
- * @param {string} message - Message to be logged.
- * @param {string} [level=DEBUG] - Level of the log message. Valid levels: DEBUG, INFO, WARN, ERROR, FATAL, SUCCESS.
- * They will not be validicated until it is written into database.
+ * @param message - Message to be logged.
+ * @param level - Level of the log message.
+ * They will not be validated until it is written into database.
  */
-schema.user.methods.log = function(message, level) {
+userSchema.methods.log = function(message: string, level: LogLevels) {
   let msg = new Log(message, level);
   this.logs.push(msg);
-}
+};
 
 /**
  * Helper class for a log message.
@@ -73,18 +80,15 @@ schema.user.methods.log = function(message, level) {
  * @prop {string} level - Level of the message.
  * @prop {string} message - Message content.
  * @prop {Date} time - Time of this message being created.
- *
- * @alias module:sms-library-helper/backend/models._Log
  */
-class Log {
-  constructor(message, level) {
-    this.level = typeof level !== 'undefined' ?  level : 'INFO';
+export class Log {
+  public level: LogLevels;
+  public time: Date;
+  public message: string;
+
+  constructor(public message: string, public level? = LogLevels.INFO) {
     this.time = new Date();
-    this.message = message;
   }
 }
 
-/** Model of the user. */
-module.exports.user = conn.model('User', schema.user);
-module.exports._schema = schema;
-module.exports._Log = Log;
+export const user = config.conn.model('User', userSchema);
