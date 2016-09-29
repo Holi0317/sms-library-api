d42e60696e4cec1c47b652d5504e79e5a04d8319
holi0317 <holliswuhollis@gmail.com>
2016-09-29 19:00:22 +0800

    FIX: cookie not saved in fetch client

--- backend/src/fetch-client.ts
+++ backend/src/fetch-client.ts
@@ -1,5 +1,6 @@
 import * as fetch from 'node-fetch';
 import * as querystring from 'querystring';
+import {CookieJar} from 'tough-cookie';
 
 interface fetchOpt {
   qs?: any;
@@ -13,20 +14,39 @@ interface fetchOpt {
 }
 
 export class FetchClient {
-  private cookie = '';
+  private cookie = new CookieJar();
   constructor() {}
+
+  private getCookie(currentUrl: string) {
+    return new Promise((resolve, reject) => {
+      this.cookie.getCookieString(currentUrl, (err, cookie) => {
+        if (err) return reject(err);
+        return resolve(cookie);
+      })
+    });
+  }
+
+  private setCookie(cookie: string, currentUrl: string) {
+    return new Promise((resolve, reject) => {
+      this.cookie.setCookie(cookie, currentUrl, (err, cookie) => {
+        if (err) return reject(err);
+        this.cookie = cookie;
+        return resolve(cookie);
+      });
+    });
+  }
 
   async fetch(url: string, opts: fetchOpt = {}) {
     if (opts.headers) {
       opts.headers = {};
     }
+    opts.headers.cookie = await this.getCookie(url);
     if (opts.qs) {
       url += `?${querystring.stringify(opts.qs)}`;
       opts.qs = undefined;
     }
-    opts.headers.cookie = this.cookie;
     let res = await fetch(url, opts);
-    // TODO Set cookies
+    await this.setCookie(await res.headers.get('Set-Cookie'), res.url);
     return res;
   }
 }
