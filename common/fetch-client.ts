4379afb7314fd08f4a259a2a3b592bac73737440
holi0317 <holliswuhollis@gmail.com>
2016-10-27 17:42:46 +0800

    FIX: fetch-client not working as intended

--- common/fetch-client.ts
+++ common/fetch-client.ts
@@ -28,25 +28,25 @@ export class FetchClient {
 
   private setCookie(cookie: string, currentUrl: string) {
     return new Promise((resolve, reject) => {
-      this.cookie.setCookie(cookie, currentUrl, (err, cookie) => {
+      this.cookie.setCookie(cookie, currentUrl, err => {
         if (err) return reject(err);
-        this.cookie = cookie;
-        return resolve(cookie);
+        return resolve();
       });
     });
   }
 
   async fetch(url: string, opts: fetchOpt = {}) {
-    if (!opts.headers) {
-      opts.headers = {};
-    }
+    opts.headers = opts.headers || {};
     opts.headers.cookie = await this.getCookie(url);
     if (opts.qs) {
       url += `?${querystring.stringify(opts.qs)}`;
       opts.qs = undefined;
     }
     let res = await fetch(url, opts);
-    await this.setCookie(await res.headers.get('Set-Cookie'), res.url);
+    let cookies = res.headers.getAll('set-cookie');
+    for (let cookie of cookies) {
+      await this.setCookie(cookie, url);
+    }
     return res;
   }
 }
