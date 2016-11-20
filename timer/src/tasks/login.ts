58a5d229cdb1d6c372204ea6a70ce03bd12d5256
holi0317 <holliswuhollis@gmail.com>
2016-11-20 20:46:16 +0800

    FEAT: Log cannot resolve DNS message to user log

--- timer/src/tasks/login.ts
+++ timer/src/tasks/login.ts
@@ -8,8 +8,13 @@ export async function login(user: CronUserData) {
   try {
     await user.library.login(user.data.libraryLogin, user.data.libraryPassword);
   } catch (err) {
-    console.error('Error when logging into library system. Error: ', err);
-    user.log('Cannot login library system. Is password changed?', 'FATAL');
+    if (err.code && err.code === 'EAI_AGAIN') {
+      user.log('Cannot resolve to library system server.', 'FATAL');
+      console.error('Error when resolving library system server. Error: ', err);
+    } else {
+      console.error('Error when logging into library system. Error: ', err);
+      user.log('Cannot login library system. Is password changed?', 'FATAL');
+    }
     user.failed = true;
     throw new Error('Failed to login library system');
   }
